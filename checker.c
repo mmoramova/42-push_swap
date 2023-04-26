fd1d90f54d2aeb1314a9825ae7b78f9ab7bcfc41
mmoramova <moramova@gmail.com>
2023-04-26 22:52:39 +0200

    code review

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: mmoramov <mmoramov@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 12:37:12 by mmoramov          #+#    #+#             */
-/*   Updated: 2023/04/26 18:14:30 by mmoramov         ###   ########.fr       */
+/*   Updated: 2023/04/26 22:51:24 by mmoramov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,10 @@ int	main(int argc, char **argv)
 		}
 		txt = ft_checker_read_input();
 		if (ft_checker_do_instructions(&a, &b, txt))
+		{
+			ft_gnl_free(&txt);
 			return (1);
+		}
 		if (ft_check_issorted(&a, &b))
 			ft_putstr_fd("OK\n", 1);
 		else
